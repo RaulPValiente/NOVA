42f2e4b8fd085a5cdc2162cbefdb4d31937a3185
Raúl <raulperezvaliente95@gmail.com>
2024-11-04 12:45:28 +0100

    navbar update

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,15 +1,16 @@
 import logo from '../assets/logo.png';
+import line from '../assets/line-h.svg';
 
 const Navbar = () => {
   return (
-    <div className="m-0 px-12 py-4 border-b border-white/10 sticky top-0 z-50 bg-[#0B030D] overflow-hidden">
+    <div className="px-12 py-4 border-b border-white/10 sticky top-0 z-50 bg-[#0B030D]">
       <nav className="flex justify-between items-center text-white">
         {/* Logo de la Empresa */}
         <div className="text-lg font-bold">
           <img src={logo} alt="NOVA Real Chain" className="w-[99px] h-[32px]" />
         </div>
 
-        <div className="flex items-center rounded-full custom-border bg-[linear-gradient(180deg,rgba(0,0,0,0)_0%,rgba(204,204,204,0.09)_20.17%,rgba(255,255,255,0.11)_100%)] shadow-[0px_1px_0px_0px_rgba(0,0,0,0.05),0px_4px_4px_0px_rgba(0,0,0,0.05),0px_10px_10px_0px_rgba(0,0,0,0.10)] backdrop-blur-[10px]">
+        <div className="flex items-center rounded-full border custom-border bg-[linear-gradient(180deg,rgba(0,0,0,0)_0%,rgba(204,204,204,0.09)_20.17%,rgba(255,255,255,0.11)_100%)] shadow-[0px_1px_0px_0px_rgba(0,0,0,0.05),0px_4px_4px_0px_rgba(0,0,0,0.05),0px_10px_10px_0px_rgba(0,0,0,0.10)] backdrop-blur-[10px]">
           <a
             href="#"
             className="px-4 py-2 text-[13px] font-normal font-medium leading-[20px]"
@@ -24,33 +25,41 @@ const Navbar = () => {
             RWA Launchpad
           </a>
 
-          <div className="relative flex items-center group">
+          <div className="relative flex items-center group z-50">
             <a
               href="#"
-              className="px-4 py-2 text-[13px] font-normal font-medium leading-[20px]"
+              className="px-4 py-2 text-[13px] font-normal font-medium leading-[20px] z-50"
             >
               Other Products
             </a>
 
-            <div className="absolute left-0 top-full mt-1 w-40 bg-white shadow-lg rounded-md hidden group-hover:block">
-              <a
-                href="#"
-                className="block px-4 py-2 text-[13px] text-gray-700 hover:bg-gray-100"
-              >
-                RWA Nodes
-              </a>
-              <a
-                href="#"
-                className="block px-4 py-2 text-[13px] text-gray-700 hover:bg-gray-100"
-              >
-                RWA Real Estate
-              </a>
-              <a
-                href="#"
-                className="block px-4 py-2 text-[13px] text-gray-700 hover:bg-gray-100"
-              >
-                RWA Miners
-              </a>
+            <div className="absolute left-0 top-full mt-0 p-4 w-[182px] bg-[#040105] shadow-lg rounded-md hidden group-hover:block z-50 custom-border ">
+              <div className="flex flex-col gap-1">
+                
+                <a
+                  id="menu"
+                  href="#"
+                  className="block px-4 py-1.5 text-[13px] border-l-[1px] border-transparent text-white z-50 menu"
+                >
+                  RWA Nodes
+                </a>
+                <img src={line} alt="" />
+                <a
+                  id="menu"
+                  href="#"
+                  className="block px-4 py-2 text-[13px] border-l-[1px] border-transparent text-white z-50 menu"
+                >
+                  RWA Real Estate
+                </a>
+                <img src={line} alt="" />
+                <a
+                  id="menu"
+                  href="#"
+                  className="block px-4 py-2 text-[13px] border-l-[1px] border-transparent text-white z-50 menu"
+                >
+                  RWA Miners
+                </a>
+              </div>
             </div>
           </div>
 
